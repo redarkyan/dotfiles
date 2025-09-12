9d981343b7ad3aa2dde0cfb65cfda0a97e00cd97
Romain Ninot <romain.ninot@datadoghq.com>
2025-09-12 10:37:43 +0200

    NVIM

---
+++ nvim/lua/community.lua
@@ -0,0 +1,14 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  "pabloariasal/webify.nvim",
+  { "hrsh7th/vim-vsnip" },
+  -- import/override with your plugins folder
+}
